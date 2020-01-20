10dc0676df1f1affa122688a72c7dbaf5729c611
mitkoevoets <mitkoevoets@vaerens.com>
2020-01-20 19:53:06 +0100

    Show error demo notification

--- src/app/containers/Advertisers/index.tsx
+++ src/app/containers/Advertisers/index.tsx
@@ -10,12 +10,11 @@ import { AdvertisersModel } from 'app/models/AdvertisersModel';
 import { Advertiser, AdvertisersActions } from 'app/actions/advertisers';
 import { RootState } from 'app/reducers';
 import { ProgressBar } from 'primereact/progressbar';
+import {Growl} from 'primereact/growl';
+import {Button} from 'primereact/button';
+import {Messages} from 'primereact/messages';
 
 const PageWrapper = styled.div`
-  //box-sizing: border-box;
-  //position: relative;
-  //min-height: 100vh;
-  //overflow: hidden;
 `;
 
 export namespace AdvertisersContainer {
@@ -36,15 +35,25 @@ export namespace AdvertisersContainer {
   }),
 )
 export class AdvertisersContainer extends React.Component<AdvertisersContainer.Props> {
+  messages: any;
+  growl: any;
 
-  onToggleMenuClick(event: any) {
-    this.setState({ layoutMode: 'overlay' });
+  constructor(props: AdvertisersContainer.Props) {
+    super(props);
+
+    this.showError = this.showError.bind(this);
   }
 
   componentDidMount() {
     const { actions } = this.props;
 
     actions.init();
+  }
+
+  showError() {
+    const msg = {severity: 'error', summary: 'Error Message', detail: 'Validation failed'};
+    this.growl.show(msg);
+    this.messages.show(msg);
   }
 
   dataTable() {
@@ -72,16 +81,23 @@ export class AdvertisersContainer extends React.Component<AdvertisersContainer.P
     };
 
     return (
-      <DataTable value={model.advertisers} paginatorPosition="both" selectionMode="single"
-                 header="List of Cars" paginator={true} rows={10}
-                 responsive={true}
-                 onSelectionChange={(event) => this.setState({ dataTableSelection: event.value })}>
-        <Column field="name" header="Advertiser" sortable={true}/>
-        <Column field="createdAt" header="Creation Date" sortable={true} sortFunction={dateSortFunction}/>
-        <Column field="campaigns" header="# Campaigns" sortable={true}/>
-        <Column field="impressions" header="Impressions" sortable={true}/>
-        <Column field="clicks" header="Clicks" sortable={true}/>
-      </DataTable>
+      <div>
+        <Messages ref={(el) => this.messages = el} style={{marginBottom: '1em'}} />
+        <Growl ref={(el) => this.growl = el} style={{marginTop: '75px'}} />
+
+        <DataTable value={model.advertisers} paginatorPosition="both" selectionMode="single"
+                   header="List of Cars" paginator={true} rows={10}
+                   responsive={true}
+                   onSelectionChange={(event) => this.setState({ dataTableSelection: event.value })}>
+          <Column field="name" header="Advertiser" sortable={true}/>
+          <Column field="createdAt" header="Creation Date" sortable={true} sortFunction={dateSortFunction}/>
+          <Column field="campaigns" header="# Campaigns" sortable={true}/>
+          <Column field="impressions" header="Impressions" sortable={true}/>
+          <Column field="clicks" header="Clicks" sortable={true}/>
+        </DataTable>
+
+        <Button onClick={this.showError} label="Show error demo notification" className="p-button-danger"  style={{width:'10em', marginRight:'.25em', marginTop: '20px'}} />
+      </div>
     );
   }
 
